73f904e61eb25b9ea88f24e0d72e6de3cf10010f
acx2o <acx2o.mm@gmail.com>
2018-06-06 16:51:26 +0900

    modified views.py

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,9 +1,11 @@
 from flaskr.twitter2 import tweet_collect
+from flaskr.youtube2 import youtube_search
 from flask import request, redirect, url_for, render_template, flash, jsonify
 from flaskr import db, app
 from flaskr.models import Artist, Song, Album
 from sqlalchemy.dialects import mysql
 
+#最初の検索画面
 @app.route('/', methods=['GET'])
 def search_artist():
     artists = Artist.query.all()
@@ -20,7 +22,7 @@ def artists():
     print(query.statement.compile(dialect=mysql.dialect(),
                                   compile_kwargs={"literal_binds": True}))
     print(artists)
-    return render_template('search_artist.html', artists=artists)
+    return render_template('search_artist_select.html', artists=artists)
 
 @app.route('/show_artist/<int:artist_id>', methods=['GET'])
 def show_artist(artist_id):
@@ -38,7 +40,11 @@ def show_artist(artist_id):
     # return jsonify(songs[0])
     return render_template('show_artist.html', artist=artist, albums=albums, songs=songs)
 
-
+@app.route('/show_video/<int:song_id>', methods=['GET'])
+def show_video(song_id):
+    song = Song.query.get(song_id)
+    video_id = youtube_search(query=song.title+' '+song.artist.name)
+    return render_template('show_video.html', video_id=video_id, song=song)
 
 @app.route('/tweet', methods=['GET'])
 def show_tweet():
